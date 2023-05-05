70d8c05aa8bedbd65c4c47cbd22540b9e8e459c9
Gloria Reina <gloria.l.reina11@gmail.com>
2023-05-05 16:03:20 -0400

    changed UTC time to EST

--- src/component/PastaDrawer.js
+++ src/component/PastaDrawer.js
@@ -1,164 +1,19 @@
-return (
-  <>
- {/* passing to EditForm so form can use the state variable and the function to allow edit button to display/hide form when clicked  */}
-{isEditFormVisible && (<EditForm 
-      />
-    )}
-    <Container>
-      <Form>
-      <div className="add-new-task-button">
-            <Button
-              bsPrefix="newtask-button"
-              variant="success"
-              onClick= {toggleAddTaskForm}
-            >
-              + New Task
-            </Button>
-      </div>
-      {isFormVisible ? <>
-        <div className="add-task-close-button">
-          <CloseButton
-            type="button"
-            onClick={toggleAddTaskForm}
-          ></CloseButton>
-        </div>
-        <Form.Group as={Row} controlId="urgency">
-          <Form.Label column sm={2} for="validationDefault01" class="form-label">
-            Urgency:
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control
-              class="form-select" 
-              id="validationDefault01" 
-              as="select"
-              required
-              value={task.urgencyLevel}
-              onChange={(evt) => {
-                const copy = { ...task };
-                copy.urgencyLevel = evt.target.value;
-                /*reset fields to default setting*/
-                update(copy);
-              }}
-            >
-              <option selected disabled value="">-- Select Urgency --</option>
-              <option value="1">Urgent and Important</option>
-              <option value="2">Urgent but not important</option>
-              <option value="3">Not Urgent but important</option>
-              <option value="4">Not Urgent and Not important</option>
-            </Form.Control>
-          </Col>
-        </Form.Group>
+const filterTasksByDate = () => {
+  const currentDate = new Date();
+  return tasks.filter((task) => {
+    const dueDate = new Date( task.deadline);
+     
+    return (
+/* add UTC(Coordinated Universal Time) in order to have consistent time zone = avoid issues with daylight saving time changes.
 
-        <Form.Group as={Row} controlId="category">
-          <Form.Label column sm={2} for="validationDefault02" class="form-label">
-            Category:
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control
-              class="form-select" 
-              id="validationDefault02" 
-              as="select"
-              required
-              value={task.category}
-              onChange={(evt) => {
-                const copy = { ...task };
-                copy.category = evt.target.value;
-                update(copy);
-              }}
-            >
-              <option selected disabled value="">-- Select Category --</option>
-              <option value="1">Self care</option>
-              <option value="2">Family life</option>
-              <option value="3">Work life</option>
-            </Form.Control>
-          </Col>
-        </Form.Group>
-        <Form.Group className="task-form-group">
-          <Form.Label className="task-form-label" for="validationDefault03" class="form-label">Description:</Form.Label>
-          <Form.Control  
-            class="form-control" 
-            id="validationDefault03" 
-            type="text"
-            required
-            placeholder="Enter task description"
-            value={task.description}
-            onChange={(evt) => {
-              const copy = { ...task };
-              copy.description = evt.target.value;
-              update(copy);
-            }}
-          />
-        </Form.Group>
-
-        <Form.Group className="task-form-group">
-          <Form.Label className="task-form-label" for="validationDefault04" class="form-label">Deadline:</Form.Label>
-          <Form.Control
-            class="form-select" 
-            id="validationDefault04" 
-            type="date"
-            required
-            placeholder="Enter Due Date"
-            value={task.deadline}
-            onChange={(evt) => {
-              const copy = { ...task };
-              copy.deadline = evt.target.value;
-              update(copy);
-            }}
-            
-          />
-        </Form.Group>
-        <Form.Group className="task-form-group">
-          <Form.Label className="task-form-label" for="validationDefault05" class="form-label">Estimated Time:</Form.Label>
-          <Form.Control
-            class="form-control" 
-            id="validationDefault05" 
-            type="text"
-            required
-            placeholder="Length of time needed (min) "
-            value={task.estimatedTime}
-            onChange={(evt) => {
-              const copy = { ...task };
-              copy.estimatedTime = evt.target.value;
-              update(copy);
-            }}
-          />
-        </Form.Group>
-        <Form.Group className="task-form-group">
-          <Form.Label className="task-form-label" for="validationDefault06" class="form-label">
-            Completion Time:
-          </Form.Label>
-          <Form.Control
-            class="form-control" 
-            id="validationDefault06"
-            type="text"
-            required
-            placeholder="Length of time used (min) "
-            value={task.actualTime}
-            onChange={(evt) => {
-              const copy = { ...task };
-              copy.actualTime = evt.target.value;
-              update(copy);
-            }}
-          />
-        </Form.Group>
-
-        <Button
-          variant="success"
-          bsPrefix="submit-task-button"
-          onClick={(clickEvent) => {
-            handleSubmitButtonClick(clickEvent);
-          }}
-        >
-          Submit Task
-        </Button>
-        </>
-        :""
-        }
-      </Form>
-    </Container>
-  </>
-);
-
+the getDate method returns the day of the month based on the local time zone of the computer= the tasks for the current day were not  displaying because the current day dates in my database where being converted to be a day behind
+*/
+      dueDate.getUTCDate() === currentDate.getUTCDate() &&
+      dueDate.getUTCMonth() === currentDate.getUTCMonth() &&
+      dueDate.getUTCFullYear() === currentDate.getUTCFullYear()
+    );
+  });
+};
 
 
 
--- src/component/task/TodayTasks.js
+++ src/component/task/TodayTasks.js
@@ -1,12 +1,8 @@
-import { EditForm } from './EditForm';
-import './TodayTasks.css';
-
-
-
+import { EditForm } from "./EditForm";
+import "./TodayTasks.css";
 
 export const TodayTasks = ({ tasks, fetchAllTasks, setTasks }) => {
-
-   /*value of taskId(can have any name) comes from  task.id property of the task object thats selected when user click its checkbox
+  /*value of taskId(can have any name) comes from  task.id property of the task object thats selected when user click its checkbox
 
   updatedTask array has all the original tasks, but with the completed status of the task with the matching taskId set to true */
   const handleTaskCompletion = (taskId) => {
@@ -18,14 +14,13 @@ export const TodayTasks = ({ tasks, fetchAllTasks, setTasks }) => {
     });
 
     //setTask prop from HomePage to update state var to updatedTasks so that on the homepage when user checks checkbox the state variable is updated so that the completed property is set to true and the task is removed from view
-    setTasks(updatedTasks)
+    setTasks(updatedTasks);
 
-    
     /*find the updated task with the matching taskId using the find method. */
     const updatedTask = updatedTasks.find((task) => task.id === taskId);
 
     /*Then sends a PUT request to update the status of that task on the server using the fetch function.*/
-    
+
     fetch(`http://localhost:8088/tasks/${taskId}`, {
       method: "PUT",
       headers: {
@@ -37,59 +32,68 @@ export const TodayTasks = ({ tasks, fetchAllTasks, setTasks }) => {
       fetchAllTasks();
     });
   };
-  
- 
+
   /*Function that filters the list of tasks based on the current date & 
     returns a new array that contains only the tasks that are due on the current date.*/
 
+  /* At first was converting to UTC,but my computer is in EST & had to modify the code to account for the 4-hour time difference between EST and UTC so tasks due on the current day didnt disappear at 8pm.*/
+
+  /*used toLocaleString() method to convert the current date and the task deadline to EST time before comparing them so format/time is the same and there's no discrepancies*/
+
   const filterTasksByDate = () => {
     const currentDate = new Date();
-    return tasks.filter((task) => {
-      const dueDate = new Date( task.deadline);
-       
-      return (
-/* add UTC(Coordinated Universal Time) in order to have consistent time zone = avoid issues with daylight saving time changes.
+    const options = { timeZone: "America/New_York" }; // set timezone to EST
+    const currentESTDate = new Date(
+      currentDate.toLocaleString("en-US", options)
+    );
 
-the getDate method returns the day of the month based on the local time zone of the computer= the tasks for the current day were not  displaying because the current day dates in my database where being converted to be a day behind
-*/
-        dueDate.getUTCDate() === currentDate.getUTCDate() &&
-        dueDate.getUTCMonth() === currentDate.getUTCMonth() &&
-        dueDate.getUTCFullYear() === currentDate.getUTCFullYear()
+    return tasks.filter((task) => {
+      const dueDate = new Date(task.deadline);
+      const dueESTDate = new Date(dueDate.toLocaleString("en-US", options));//"en-US"=language specific format, timeZone option= supports IANA/Olson time zones, such as America/New_York or Europe/London & allows you to work with timezones other than UTC or your own local time zone
+
+      return (
+        dueESTDate.getDate() === currentESTDate.getDate() &&
+        dueESTDate.getMonth() === currentESTDate.getMonth() &&
+        dueESTDate.getFullYear() === currentESTDate.getFullYear()
       );
     });
   };
 
   let filteredTasks = filterTasksByDate();
-  filteredTasks = filteredTasks.filter(task=>{ return task.completed===false})
+  filteredTasks = filteredTasks.filter((task) => {
+    return task.completed === false;
+  });
 
   /*  new array (sortedFilteredTasks) takes a copy of the (filteredTask array) which holds tasks due on current day and sorts these tasks by urgency level.
   
   a and b = two elements of the array being compared, if a-b<0, a is displayed first, if >0, b is displayed first, if = 0 nothing changes
   */
 
-  const sortedFilteredTasks = [...filteredTasks].sort((a, b) => a.urgencyLevel - b.urgencyLevel);
-
+  const sortedFilteredTasks = [...filteredTasks].sort(
+    (a, b) => a.urgencyLevel - b.urgencyLevel
+  );
 
   return (
     <>
-    <div>
-      <h1>Tasks due today:</h1>
-      <ul>
-        {sortedFilteredTasks.map((task) => (
-          <>
-          {/* add a CSS class based on urgency level of each task--use to color-code tasks based off */}
-           <div className={`task-item urgency-${task.urgencyLevel}`}>
-          <li key={task.id}>{task.description} </li>
-          
-          </div>
-          <EditForm task={task}
-                  fetchAllTasks={fetchAllTasks}
-                  handleTaskCompletion={handleTaskCompletion}
-                  //  //pass fetchALLTasks prop EditForm component so that it can use it after the PUT request to update page with current state; and task so it can have access to task object for edits
-                  />
-          </>
-        ))}
-      </ul>
-    </div>
-  </>
-)}
+      <div>
+        <h1>Tasks due today:</h1>
+        <ul>
+          {sortedFilteredTasks.map((task) => (
+            <>
+              {/* add a CSS class based on urgency level of each task--use to color-code tasks based off */}
+              <div className={`task-item urgency-${task.urgencyLevel}`}>
+                <li key={task.id}>{task.description} </li>
+              </div>
+              <EditForm
+                task={task}
+                fetchAllTasks={fetchAllTasks}
+                handleTaskCompletion={handleTaskCompletion}
+                //  //pass fetchALLTasks prop EditForm component so that it can use it after the PUT request to update page with current state; and task so it can have access to task object for edits
+              />
+            </>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
